edccd403e7d6f98d91d8aa0b634e862b82f1d426
KhatriSuson <susonkhatri110@gmail.com>
2024-11-21 15:19:02 +0545

    update install app and middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'accounts',
+    'credentials',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+     #react dev server
+    "http://locathost:3000',
+   
 ]
 
 ROOT_URLCONF = 'backend.urls'
