0cd0b8336a1c2ab1aba1bc2ae2cec1cbaaac460f
KhatriSuson <susonkhatri110@gmail.com>
2024-11-21 15:45:20 +0545

    create serializer.py for RestAPI

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from accounts.models import UserProfile
+from credentials.models import Credential
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Credential)
